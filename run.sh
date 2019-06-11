86314f442a4e63591399a4691a45d888d431c6ef
Stanislav Blokhin <stanislav.blokhin@tomologic.com>
2019-06-11 04:32:26 +0200

    Upgrade pip packages

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ BROKER_API=${BROKER_API}
 
 echo "Using broker URL: ${BROKER_URL}"
 
-if [ ! -z "${BROKER_API}" ]; then
+if [ -n "${BROKER_API}" ]; then
     echo "Using broker API URL: ${BROKER_API}"
     API_OPTION="--broker_api=${BROKER_API}"
 fi
 
 exec celery flower \
-    --broker=${BROKER_URL} ${API_OPTION} \
+    --broker="${BROKER_URL}" ${API_OPTION} \
     --address=0.0.0.0 --port=8080
