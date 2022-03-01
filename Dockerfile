457d1df378afd03e49107b100ac2f80892d8121f
Stanislav Blokhin <stanislav.blokhin@tomologic.com>
2022-03-01 21:16:04 +0100

    Upgrade dependencies
    Python 3.10
    Newer versions of dependencies
    Instead of Alpine Linux use Ubuntu to quicker install Python packages as
    wheels.
    
    Docker image is now built using Github Actions

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
-FROM python:3.6-alpine
+FROM python:3.10
 EXPOSE 8080
-
-# Requirements to compile hiredis
-RUN apk add gcc musl-dev --no-cache
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY requirements.txt /usr/src/app/
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt && pip check
 
 COPY run.sh /usr/src/app/
 CMD ["/usr/src/app/run.sh"]
--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 BROKER_URL=${BROKER_URL:-amqp://guest:guest@rabbitmq//}
-BROKER_API=${BROKER_API}
 
-echo "Using broker URL: ${BROKER_URL}"
+echo "Using broker URL: $BROKER_URL"
 
-if [ -n "${BROKER_API}" ]; then
-    echo "Using broker API URL: ${BROKER_API}"
-    API_OPTION="--broker_api=${BROKER_API}"
+if [ -n "$BROKER_API" ]; then
+    echo "Using broker API URL: $BROKER_API"
+    API_OPTION="--broker_api=$BROKER_API"
 fi
 
-exec celery flower \
-    --broker="${BROKER_URL}" ${API_OPTION} \
+exec celery --broker="$BROKER_URL" \
+    flower "$API_OPTION" \
     --address=0.0.0.0 --port=8080
